ebfd9a81bd5d83f1132694a00ebfb26f7e62893f
corvuslesgrange <58277328+corvuslesgrange@users.noreply.github.com>
2020-04-06 20:27:50 -0300

    Create comicbook.go

---
+++ comicbook.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var publisher string
+  var writer string
+  var artist string
+  var title string
+  var year int
+  var pageNumber int
+  var grade float32
+
+  title = "Mr GoToSleep"
+  writer = "Tracey Heatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc"
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+
+fmt.Println(title, "written by", writer, "drawn by", artist, "publish by", publisher, "year", year, "pagenumber", pagenumber, "grade", grade)
+
+title = "Epic Vol.1"
+writer = "Ryan N. Shawn"
+artist = "Phoebe Paperclips"
+publisher = "DizzyBooks Publishing Inc"
+year = 2013
+pageNumber = 160
+grade = 9.0
+
+fmt.Println(title, "written by", writer, "drawn by", artist, "publish by", publisher, "year", year, "pagenumber", pagenumber, "grade", grade)
+}
+
