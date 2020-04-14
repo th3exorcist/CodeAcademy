2617b7837193224fac0816cb5ce44dfe049b93f2
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-04-14 11:22:37 -0300

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+#  Greeting a user
+print('Hello shadow hunter')
+#   Informing the user that they are interacting with a bot (and not a human)
+print('I will repeat after you')
+# Instructing the user on how to interact with the bot
+stuff_to_echo = input('Enter something for me to repeat: ')
+# Setting up output that is dependent on user input
+print('you said: ' + stuff_to_echo)
