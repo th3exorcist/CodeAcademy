0cbf3b9c9097832e4436af3c4536708384f0e3be
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-04-20 16:52:28 -0300

    Create plant_care.py

---
+++ plant_care.py
@@ -0,0 +1,18 @@
+from adafruit_circuitplayground.express import cpx
+import touchio
+touch = touchio.TouchIn(A1)
+
+while True:
+  if cpx.switch:
+    if cpx.button_a:
+      print("Temperature: ", cpx.temperature)
+    if cpx.button_b:
+      print("Light value: ", cpx.light)
+  else:
+    if cpx.button_a:
+      print("Moisture:", touch.raw_value)
+    if cpx.button_b:
+      if touch.raw_value > 1500:
+        print("Water me!")
+      else:
+        print("I'm good, thanks!")
