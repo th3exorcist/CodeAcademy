b3ead1cd0236dca08f016239d2312e0f0c1ea8d5
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-04-24 19:00:47 -0300

    Update distinct.sql

--- distinct.sql
+++ distinct.sql
@@ -1,1 +1,9 @@
-SELECT DISTINCT year FROM movies;
+SELECT DISTINCT year FROM movies;  
+
+SELECT name,
+ CASE
+  WHEN genre = 'romance' THEN 'Chill'
+  WHEN genre = 'comedy'  THEN 'Chill'
+  ELSE 'Intense'
+ END AS 'Mood'
+FROM movies;
