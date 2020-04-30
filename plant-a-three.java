5b7e78d277db392b1615ac600c46abc02f8b08ca
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-04-30 15:41:38 -0300

    Create plant-a-three.java

---
+++ plant-a-three.java
@@ -0,0 +1,16 @@
+public class Tree {
+
+   public static void main(String[] args) {
+    // We’re going to introduce ourselves and print a tree to the screen. 
+    System.out.println("Hi, I'm Batman");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println("   *   ");
+    System.out.println("   *   "); 
+    System.out.println(" ######  "); 
+  }
+
+
+}
