3f19418461705db7493a61008362ef3d310b9843
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-05-20 15:10:05 -0300

    Create creatures.cs

---
+++ creatures.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // creatures
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |"); 
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("boooh");
+     }
+  }
+}
