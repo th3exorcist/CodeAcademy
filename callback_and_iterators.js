da592eb5184ebb5dae5574cde5d40fe6f1f92ca1
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-05-29 11:51:54 -0300

    Update callback_and_iterators.js

--- callback_and_iterators.js
+++ callback_and_iterators.js
@@ -92,3 +92,16 @@ const startsWithS = animals.findIndex(animal => {
   return animal[0] === 's' ? true : false;
 });
 
+// reduce
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
+
+
