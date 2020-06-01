9de0a4c0bf0e8993e2df64ce672d9355027b09c9
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-06-01 12:48:25 -0300

    gradebook

---
+++ gradebook.py
@@ -0,0 +1,28 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+
+grades = [98,97,85,88];
+
+gradebook = list(zip(subjects, grades))
+
+
+print(gradebook)
+
+subjects.append("computer science")
+
+grades.append(100)
+
+gradebook = list(zip(subjects, grades))
+
+print(gradebook)
+
+gradebook.append(("visual arts", 93))
+
+print(gradebook)
+
+full_gradebook = gradebook + last_semester_gradebook
+
+print("----------")
+print(full_gradebook)
